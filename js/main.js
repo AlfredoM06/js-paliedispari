7b6f2ca9dea97e7720d336ad2d57e108040814ad
AlfredoM06 <Alfredomanfredi06@gmail.com>
2023-11-20 15:20:05 +0100

    started repo with index, folders and logic

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// l'esercizio consiste nel risolvere due casi: il primo dobbiamo riuscire 
+// a far si che si tramite una funzione si riconosca se la parola inserita 
+// dall'utente sia palindroma.
+// il secondo caso invece ci chiede di fare la somma di due numeri uno inserito
+// dall'utente e uno random e l'utente deve indovinare se è pari o dispari
+// fare il tutto tramite funzione e dire se è ha vinto o meno
+
+
+
+
+
+// inzio: caso palindromo o meno, raggionamento:
+// inzio chiedendo le informazioni all'utente 
+// poi creare devo creare una funzione che mi legga la parola e veda se è palindorma o meno
+// in fine devo comunicare all'utente se lo è o no
+
+
+
+
+
+
+
+// inizio: caso pari o dispari, raggionamento:
+// inzio sempre chiedendo info all'utente facendogli scegliere pari o dispari
+// di seguito gli chiedo di inserire un numero e generarne uno randomico
+// devo fare una funzione che mi sommi i due numeri e controlli che siano pari o dispari
+// in fine devo comunicare che ha vinto.
